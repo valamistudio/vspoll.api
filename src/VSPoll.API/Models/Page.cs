bc8beaf3a43ad471c68748030932d07262244bae
Bruno Logerfo <bclogerfo@hotmail.com>
2020-05-17 14:50:32 -0300

    paging structures

---
+++ src/VSPoll.API/Models/Page.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VSPoll.API.Models
+{
+    public class Page<TItem>
+    {
+        public int Size { get; set; }
+
+        public int Number { get; set; }
+
+        public int TotalItems { get; set; }
+
+        public IEnumerable<TItem> Items { get; set; } = null!;
+
+        public int TotalPages => Size > 0 ? (int)Math.Ceiling((decimal)TotalItems / Size) : 0;
+
+        public Page<TDestination> Map<TDestination>(Func<TItem, TDestination> mapFunction) => new Page<TDestination>
+        {
+            Items = Items.Select(mapFunction),
+            Number = Number,
+            Size = Size,
+            TotalItems = TotalItems,
+        };
+    }
+}
---
+++ src/VSPoll.API/Models/Paged.cs
@@ -0,0 +1,36 @@
+﻿namespace VSPoll.API.Models
+{
+    public class Paged
+    {
+        private const int DEFAULT_SIZE = 3;
+        private const int MAX_SIZE = 50;
+
+        private int page;
+        public int Page
+        {
+            get => page;
+            set
+            {
+                if (value < 1)
+                    page = 1;
+                else
+                    page = value;
+            }
+        }
+
+        private int pageSize;
+        public int PageSize
+        {
+            get => pageSize;
+            set
+            {
+                if (value < 0)
+                    pageSize = DEFAULT_SIZE;
+                else if (value > MAX_SIZE)
+                    pageSize = MAX_SIZE;
+                else
+                    pageSize = value;
+            }
+        }
+    }
+}
